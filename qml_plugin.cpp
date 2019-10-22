cc6e93bcb007f1bfaa6522aaa6f6ae273d55ba26
pchdev <pchd@quarre.fr>
2019-10-22 09:36:45 +0200

    module update

--- qml_plugin.cpp
+++ qml_plugin.cpp
@@ -1,17 +1,20 @@
 #include "qml_plugin.hpp"
 
-#include <source/hlpf/filter.hpp>
+//#include <source/hlpf/filter.hpp>
 #include <source/mangler/mangler.hpp>
-#include <source/sharpen/sharpen.hpp>
 
 #include <QQmlEngine>
 #include <qqml.h>
 
-void qml_plugin::registerTypes(const char *uri)
+//-------------------------------------------------------------------------------------------------
+void
+qml_plugin::registerTypes(const char *uri)
+//-------------------------------------------------------------------------------------------------
 {
-    Q_UNUSED    ( uri );
+    Q_UNUSED(uri)
 
-    qmlRegisterType<Sharpen, 1> ( "WPN114.Audio.JSEffects", 1, 0, "Sharpen" );
-    qmlRegisterType<Mangler, 1> ( "WPN114.Audio.JSEffects", 1, 0, "Mangler" );
-    qmlRegisterType<Filter, 1>  ( "WPN114.Audio.JSEffects", 1, 0, "HLPFilter" );
+    qmlRegisterType<Mangler, 1>
+    ("WPN114.Audio.Js", 1, 1, "Mangler");
+
+//    qmlRegisterType<Filter, 1>  ( "WPN114.Audio.JSEffects", 1, 0, "HLPFilter" );
 }
